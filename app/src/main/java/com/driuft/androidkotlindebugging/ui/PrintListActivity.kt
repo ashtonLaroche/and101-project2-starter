a12f0cc3282ba198a3e6cd9866c31b660b52b4ba
Ashton <awl190001@utdallas.edu>
2023-09-25 23:01:09 -0500

    final sub

--- app/src/main/java/com/driuft/androidkotlindebugging/ui/PrintListActivity.kt
+++ app/src/main/java/com/driuft/androidkotlindebugging/ui/PrintListActivity.kt
@@ -5,7 +5,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.driuft.androidkotlindebugging.R
 
-class PrintListActivity : AppCompatActivity() {
+class   PrintListActivity : AppCompatActivity() {
 
     private val wordList: TextView get() = findViewById(R.id.word_list)
     private var words = listOf(
@@ -26,7 +26,7 @@ class PrintListActivity : AppCompatActivity() {
 
     private fun combinedWords(): String {
         var combined = ""
-        for (idx in 0 until words.size + 1) {
+        for (idx in 0 until words.size) {
             combined += "${words[idx]}\n"
         }
         return combined
